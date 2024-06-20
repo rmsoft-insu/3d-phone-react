8b5fb5acf3de52e0aa44979b6abf38582dc11cea
insu <hinsu@akiaka.co.kr>
2024-06-20 16:45:23 +0900

    add prettier plugin tailwind

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,23 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
 export const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <header className="flex w-full px-5 py-5 sm:px-10">
+        <nav>
+          <img src={appleImg} alt="Apple" width={14} height={18} />
+          <div>
+            {["Phones", "Macbooks", "Tablets"].map((nav) => (
+              <div key={nav}>{nav}</div>
+            ))}
+          </div>
+
+          <div>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
