d2b870bdba889344dc3a3537fb75446e6ec2c398
insu <hinsu@akiaka.co.kr>
2024-06-20 17:27:32 +0900

    update highlights

--- src/components/highlights.jsx
+++ src/components/highlights.jsx
@@ -1,7 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 export const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+  });
   return (
     <>
-      <div>Highlights</div>
+      <section
+        id="highlights"
+        className="common-padding h-full w-screen overflow-hidden bg-zinc"
+      >
+        <div className="screen-max-width">
+          <div className="mb-12 w-full items-end justify-between">
+            <h1 id="title" className="section-heading">
+              Get the highlights.
+            </h1>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
