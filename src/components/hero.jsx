93da6d2f48f4af49f2731285c3e34dda77b98700
insu <hinsu@akiaka.co.kr>
2024-06-20 17:06:14 +0900

    add first gsap animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,20 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 export const Hero = () => {
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
+
   return (
     <>
-      <div>Hero</div>
+      <section className="nav-height relative w-full bg-black">
+        <div className="flex-center h-5/6 w-full flex-col">
+          <p id="hero" className="hero-title">
+            iPhone 15 Pro
+          </p>
+        </div>
+      </section>
     </>
   );
 };
