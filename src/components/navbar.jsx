e8ad38266cef99896fdc3e2bf6ddc4b9c2e25e64
insu <hinsu@akiaka.co.kr>
2024-06-20 16:55:31 +0900

    update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,24 @@
 import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
 
 export const Navbar = () => {
   return (
     <>
-      <header className="flex w-full px-5 py-5 sm:px-10">
-        <nav>
+      <header className="flex w-full items-center justify-between px-5 py-5 sm:px-10">
+        <nav className="screen-max-width flex w-full">
           <img src={appleImg} alt="Apple" width={14} height={18} />
-          <div>
-            {["Phones", "Macbooks", "Tablets"].map((nav) => (
-              <div key={nav}>{nav}</div>
+          <div className="flex flex-1 justify-center max-sm:hidden">
+            {navLists.map((nav) => (
+              <div
+                key={nav}
+                className="cursor-pointer px-5 text-sm text-gray transition-all hover:text-white"
+              >
+                {nav}
+              </div>
             ))}
           </div>
 
-          <div>
+          <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
             <img src={searchImg} alt="search" width={18} height={18} />
             <img src={bagImg} alt="bag" width={18} height={18} />
           </div>
