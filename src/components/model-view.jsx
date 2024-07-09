c71027ecba2a999601e22c4f27c77ba4c9fdb531
insu <hinsu@akiaka.co.kr>
2024-07-09 14:00:14 +0900

    아이폰 3d 모델 불러오기

--- src/components/model-view.jsx
+++ src/components/model-view.jsx
@@ -1,7 +1,35 @@
-export const ModelView = () => {
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./lights";
+import IPhone from "./iPhone";
+import { Suspense } from "react";
+
+export const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
     <>
-      <div>ModelView</div>
+      <View
+        index={index}
+        id={gsapType}
+        className={`h-full w-full ${index === 2} ? 'right-[-100%]':''`}
+      >
+        {/* Amient Light */}
+        <ambientLight intensity={0.3} />
+
+        <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+        <Lights />
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <IPhone />
+        </Suspense>
+      </View>
     </>
   );
 };
